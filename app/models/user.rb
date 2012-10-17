ac1eabaf780d68c7ff2c9f8ed97205057f1589ea
Nikolay Petrachkov <nik@jastix.biz>
2012-10-17 09:45:59 +0200

    Cleanup

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :email, :name
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name,  presence: true, length: { maximum: 25 }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
-end
