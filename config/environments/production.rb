506701f9a4add023702a41b7d2c1e9ac1d9ab151
Nikolay Petrachkov <nik@jastix.biz>
2012-10-22 14:24:36 +0200

    Add email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,18 @@ OneQuote::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # ActionMAiler config
+  config.action_mailer.default_url_options = { host: 'smtp.mandrillapp.com' }
+  config.action_mailer.delvery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: 'utf-8'
+
+  config.action_mailer.smtp_settings = {
+    address:   'smtp.mandrillapp.com',
+    port:      '587',
+    user_name: ENV['MANDRILL_USERNAME'],
+    password:  ENV['MANDRILL_API_KEY']
+  }
 end
